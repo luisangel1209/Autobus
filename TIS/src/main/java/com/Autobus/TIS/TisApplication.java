d9771ce82b1aee1eb4389238710c437d89857020
LUIS <LUIS@LAPTOP-LNP6PGD9>
2020-04-17 14:25:00 -0500

    Base de datos implementada, faltan cosas

--- TIS/src/main/java/com/Autobus/TIS/TisApplication.java
+++ TIS/src/main/java/com/Autobus/TIS/TisApplication.java
@@ -3,6 +3,8 @@ package com.Autobus.TIS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import Conexion.ConexionBD;
+
 @SpringBootApplication
 public class TisApplication {
 
